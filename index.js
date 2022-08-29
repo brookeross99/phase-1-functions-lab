b392c94132377cad1cad1755c8b7a3a87df00c0e
Brooke Ross <brooke.ross2800@gmail.com>
2022-08-29 17:34:59 -0600

    commit tree a2dc39e2186cb81dd77d05aa5c55f85f60a236f3

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue){
+    return someValue-`42`
+    if(distanceFromHqInBlocks(someValue)>='42'){
+       return someValue -'42'
+    }else if (someValue<=`42`){
+        return `42`- someValue
+    }
+}
